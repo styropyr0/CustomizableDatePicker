090039c1e6bc7ccad25770c64938b01dc6a75c19
saurav <sauravsajeev202@gmail.com>
2024-11-21 23:18:07 +0530

    Publish commit

--- styro_custom-date-picker/build.gradle.kts
+++ styro_custom-date-picker/build.gradle.kts
@@ -48,7 +48,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.styropyr0"
                 artifactId = "styro-custom-date-picker"
-                version = "1.0.0"
+                version = "1.0.2"
             }
         }
     }
